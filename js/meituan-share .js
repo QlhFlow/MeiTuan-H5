994f8646ab07842d5644a2693a0256fbb6fcc60c
selina1202 <yuze1202@sina.com>
2017-04-10 16:52:15 +0800

    重新注册

--- js/meituan-share .js
+++ js/meituan-share .js
@@ -2,7 +2,7 @@ var isInited = false; //初始化状态
 var shareConfig = {
 	channel: 385,
 		content: '4月健康餐——X008食谱，坐享轻松之路',
-		href: '',
+		href: 'https://h5.xingyuanauto.com/Dongbiao/DbMeit/index.html',
 		imageURL: 'https://h5.xingyuanauto.com/Dongbiao/DbMeit/img/xiaotu.png',
 		title: '东风标致X008 SUV三重钜惠豪华盛宴',
 		success: null
