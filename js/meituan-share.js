4b42329e4db939b177ee1f4f9028b31f29099c6f
selina1202 <yuze1202@sina.com>
2017-04-11 12:06:12 +0800

    VC徐晓晨

---
+++ js/meituan-share.js
@@ -0,0 +1,58 @@
+/**
+ * Created by jiangqian on 2017/4/11.
+ */
+
+var isInited = false; //初始化状态
+var shareConfig = {
+    channel: '385',
+    content: '4月健康餐——X008食谱，坐享轻松之路',
+    href: 'https://h5.xingyuanauto.com/Dongbiao/DbMeit/index.html',
+    imageURL: 'https://h5.xingyuanauto.com/Dongbiao/DbMeit/img/xiaotu.jpg',
+    title: '东风标致X008 SUV三重钜惠豪华盛宴',
+    success: null
+}; //配置参数
+function ready(callback) {
+    function init (bridge) {
+        if (isInited) return;
+        try {
+            bridge.init();
+            isInited = true;
+        } catch (e) {
+            console.log('init failed');
+        }
+    }
+    // 如果有WebViewJavascriptBridge 这个全局变量，
+    // 则直接初始化它
+    if (window.WebViewJavascriptBridge) {
+        init(WebViewJavascriptBridge);
+        callback(WebViewJavascriptBridge);
+        return;
+    }
+
+    // 否则添加监听事件再初始化
+    document.addEventListener('WebViewJavascriptBridgeReady', function () {
+        init(WebViewJavascriptBridge);
+        callback(WebViewJavascriptBridge);
+    }, false);
+}
+//设置分享
+function setShare(shareConfig) {
+    ready(function (bridge) {
+        bridge.callHandler('callNativeMethod', {
+            "moduleName": "platform",
+            "methodName": "shareCommon",
+            "data": {
+                "channel": shareConfig.channel,//频道十进制值相加
+                "content": shareConfig.content,//分享内容 兼容android 该字段必须有内容
+                "content_-1": shareConfig.content,
+                "content_1": true,
+                "detailURL": shareConfig.href,//分享的详情链接
+                "imageURL": shareConfig.imageURL,//分享的缩略图链接
+                "title": shareConfig.title//分享的标题
+            }
+        }, function (res) {
+            shareConfig.success && shareConfig.success(res);
+        });
+    });
+}
+setShare(shareConfig);//调用设置分享的方法
